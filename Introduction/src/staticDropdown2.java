d8d698ece63756df58a747d219517e0b70d53bfd
deepakdm <deepakdm2016@gmail.com>
2019-05-03 20:38:10 +0530

    Java reference May 03rd

--- Introduction/src/staticDropdown2.java
+++ Introduction/src/staticDropdown2.java
@@ -1,13 +1,14 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class staticDropdown2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Deepak\\chromedriver.exe");
@@ -16,11 +17,28 @@ public class staticDropdown2 {
 		driver.get("https://www.spicejet.com/");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		Select select=new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
+		
+		
 		select.selectByIndex(3);
 		select.selectByValue("AED");
 		select.selectByVisibleText("INR");
 		select.deselectByIndex(3);
 		
+		for(int i=0;i<10000;i++)
+		{
+		
+			try{
+			Thread.sleep(1000);
+			select.selectByIndex(i);
+			String text = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")).getText();
+			System.out.println(text);
+			}
+			catch(NoSuchElementException e)
+			{
+				e.printStackTrace();
+				break;
+			}
+		}
 		driver.quit();
 
 	}
