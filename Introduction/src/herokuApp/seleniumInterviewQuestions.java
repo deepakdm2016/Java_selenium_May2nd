ac8a17d187bab5d6a1173227f6ea1fa419a9a18e
deepakdm2016 <deepakdm2016@gmail.com>
2019-05-21 06:35:38 +0530

    commiting the files 21st may

--- Introduction/src/herokuApp/seleniumInterviewQuestions.java
+++ Introduction/src/herokuApp/seleniumInterviewQuestions.java
@@ -12,22 +12,22 @@ public class seleniumInterviewQuestions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Deepak\\Selenium\\Java_selenium_May2nd\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Deepak\\Selenium\\Java_selenium_May2nd\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.softwaretestingmaterial.com/selenium-interview-questions/");
-
-		String text = driver.findElement(By.xpath("//strong[contains(text(),'?')]")).getText();
-
-		System.out.println("text: " + text);
-		List<WebElement> questions = driver.findElements(By.xpath("//strong[contains(text(),'?')]"));
-		for (int i = 0; i < questions.size(); i++) {
-
-			System.out.println(questions.get(i).getText());
-			// System.out.println("*************************************************************");
-
+		
+		String text = driver.findElement(By.xpath("//strong[contains(text(),'8. Can you explain the Framework which you have us')]")).getText();
+		
+		System.out.println("text: "+text);
+		List<WebElement> questions = driver.findElements(By.xpath("//*[contains(text(),'?')]"));
+		for(int i=0;i<questions.size();i++)
+		{
+			
+		System.out.println(questions.get(i).getAttribute("textContent"));
+		//System.out.println("*************************************************************");
+		
 		}
 
 	}
